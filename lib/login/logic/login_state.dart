29b56f01e1685c2410e5478ed6e199b08ac4032c
tanphuccgl <phamnguyentanphucgl@gmail.com>
2023-02-25 21:45:06 +0700

    add new custom file generator

---
+++ lib/login/logic/login_state.dart
@@ -0,0 +1,10 @@
+part of 'login_bloc.dart';
+
+class LoginState extends Equatable {
+  const LoginState();
+
+  @override
+  List<Object?> get props => [];
+
+  LoginState copyWith() => const LoginState();
+}
---
+++ lib/login/widgets/login_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatelessWidget {
+  const LoginWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
